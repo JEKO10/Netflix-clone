34dbe5a2d84fe742e582ac237fe9256e67fdd1d6
Aleksa Bubanja <aleksa3b@gmail.com>
2021-10-31 20:51:26 +0100

    button changes icon on click

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const input = document.querySelector("#input");
 const submit = document.querySelector("#submit");
 const inputError = document.querySelector("#inputError");
+const plusIcon = document.querySelectorAll("#plusIcon");
 
 submit.addEventListener("click", (e) => {
   e.preventDefault();
@@ -9,4 +10,11 @@ submit.addEventListener("click", (e) => {
     inputError.style.display = "block";
   }
   input.focus();
+});
+
+plusIcon.forEach((singlePlus) => {
+  singlePlus.addEventListener("click", () => {
+    singlePlus.classList.toggle("fa-plus");
+    singlePlus.classList.toggle("fa-times");
+  });
 });
